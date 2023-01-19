6b83d695cba4181fdfbb2cd8b593cfffb2a4d99a
chynmn <chynmn0523@gmail.com>
2023-01-19 22:58:56 +0900

    test13

--- deploy.sh
+++ deploy.sh
@@ -22,9 +22,11 @@ then
   sudo curl -L "https://github.com/docker/compose/releases/download/1.27.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
   sudo chmod +x /usr/local/bin/docker-compose
 fi
-
-cd /home/ubuntu/srv/ubuntu && git submodule update --init --recursive
-mv /home/ubuntu/srv/ubuntu/.env /home/ubuntu/srv/ubuntu/backend/
+echo "start submoudle update"
+cd /home/ubuntu/srv/ubuntu
+sudo git submodule init
+sudo cd /home/ubuntu/srv/ubuntu && git submodule update --init --recursive
+sudo mv /home/ubuntu/srv/ubuntu/.env /home/ubuntu/srv/ubuntu/backend/
 
 echo "start docker-compose up: ubuntu"
 sudo docker-compose -f /home/ubuntu/srv/ubuntu/docker-compose.yml up --build -d
