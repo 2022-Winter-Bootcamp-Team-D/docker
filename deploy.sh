402695122b44f75e76725ed34769f2d57d6c608b
chynmn <chynmn0523@gmail.com>
2023-01-27 18:23:44 +0900

    [hotfix] modify workflow trigger

--- deploy.sh
+++ deploy.sh
@@ -22,6 +22,7 @@ then
   sudo curl -L "https://github.com/docker/compose/releases/download/1.27.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
   sudo chmod +x /usr/local/bin/docker-compose
 fi
+
 echo "start submoudle update"
 cd /home/ubuntu/srv/ubuntu
 sudo git submodule init
